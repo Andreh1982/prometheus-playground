2602453c00a2b0f0205be2a45d1bd50a959fdd5f
André Luís Torres Pereira <andre.pereira@olist.com>
2022-06-01 18:00:51 -0300

    Pushgateway Tests

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+while :
+    do
+        echo "Sending Payload..."
+        cat test.payload | curl --data-binary @- ***
+        echo "Done!"
+        sleep 20
+        echo "Sending Payload.0"
+        cat test.payload.0 | curl --data-binary @- ***
+        echo "Done!"
+        sleep 20
+
+    done
